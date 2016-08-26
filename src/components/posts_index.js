55933a7a02a1ab6892d32dfd5b473d477f3b5cb5
sanjaysi <cloudwithpython.com>
2016-08-27 00:49:50 +0530

    90 Posts Index

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -6,20 +6,39 @@ import { Link } from 'react-router';
 class PostsIndex extends Component {
 	componentWillMount() {
 		this.props.fetchPosts();
-	}
+	};
+
+	renderPosts() {
+		return this.props.posts.map((post) => {
+			return(
+				<li className="list-group-item" key={post.id}>
+					<span className="pull-xs-right">{post.categories}</span>
+					<strong>{post.title}</strong>
+				</li>
+			);
+		})
+	};
 
 	render() {
 		return(
 			<div>
 				<div className="text-xs-right">
 					<Link to="/posts/new" className="btn btn-primary" >
-					Add a Post
+						Add a post
 					</Link>
 				</div>
+				<h3>Posts</h3>
+				<ul className="list-group">
+					{this.renderPosts()}
+				</ul>
 				List of Posts
 			</div>
 		);
 	}
 }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+function mapStateToProps(state) {
+	return { posts: state.posts.all };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
