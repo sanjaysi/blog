f321f4bc4e26f2076bebd5e05280c25b4e07fb0b
sanjaysi <cloudwithpython.com>
2016-08-27 22:52:23 +0530

    91 Loading Data on Render Added link to posts

---
+++ src/components/posts_show.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class PostShow extends Component {
+
+	render() {
+		return(
+			<div>
+				Show post {this.props.params.id}
+			</div>
+		)
+	}
+}
+
+export default PostShow;
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostShow from './components/posts_show';
 
 const Greeting = () => {
     return(
@@ -15,5 +16,6 @@ export default (
     <Route path="/" component={App} >
         <IndexRoute component={PostsIndex} />
     <Route path="posts/new" component={PostsNew} />
+    <Route path="posts/:id" component={PostShow} />
     </Route>
 );
