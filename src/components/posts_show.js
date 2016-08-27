3ebc1923b7e09b5330e331e8dcf8ef1edabed21c
sanjaysi <cloudwithpython.com>
2016-08-27 23:52:15 +0530

    95 Navigate on Delete Application Complete.

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,14 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost, deletePost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostShow extends Component {
+	static contextTypes = {
+		router: PropTypes.object
+	};
+
+	componentWillMount() {
+		this.props.fetchPost(this.props.params.id);
+	};
+
+	onDeleteClick() {
+		this.props.deletePost(this.props.params.id)
+			.then(() => { 
+				this.context.router.push("/");
+			});
+	};
 
 	render() {
+		const {post} = this.props;
+
+		if (!this.props.post) {
+			return <div>Loading...</div>;
+		}
+
 		return(
 			<div>
-				Show post {this.props.params.id}
+				<Link to="/">Back To Index</Link>
+				<button 
+					className="btn btn-danger pull-xs-right"
+					onClick={this.onDeleteClick.bind(this)} >
+					Delete Post
+				</button>
+				<h3>{post.title}</h3>
+				<h6>{post.categories}</h6>
+				<p>{post.content}</p>
 			</div>
-		)
-	}
+		);
+	};
 }
 
-export default PostShow;
+function mapStateToProps(state) {
+	return { post: state.posts.post };
+}
+
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostShow);
